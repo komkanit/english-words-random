4261dcc4dbc23bd36a4004c9eb5b08696dfdec2d
komcal <kom.sujautra@gmail.com>
2020-01-12 21:21:17 +0700

    add feature

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { data } from './data'
+
+const randomIndex = () => {
+  return Math.floor(Math.random() * data.length);
+}
 
 function App() {
+  const [wordIndex, setNewIndex] = useState(randomIndex());
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="word">{data[wordIndex]}</h1>
+        <button
+          className="random-btn"
+          onClick={() => {
+            setNewIndex(randomIndex())
+          }}
+          >Random</button>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href={`https://www.oxfordlearnersdictionaries.com/search/english/?q=${data[wordIndex].split(' ').join('+')}`}
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Search this word
         </a>
       </header>
     </div>
